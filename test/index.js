8575694a458f8375ca72db88683b83ceb736b683
Adam <adam@berrygrab.com>
2015-11-21 00:11:06 +0000

    Scaffold package using generator node

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import nodeMagento2 from '../lib';
+
+describe('node-magento2', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
